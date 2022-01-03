d3c447f06c9436fa1f621d5074c24cc656e7f030
saralimbu2017 <iamsebi7@gmail.com>
2022-01-03 17:39:35 +1100

    Fixing typos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,30 +31,7 @@
         <x-home-search></x-home-search>
                   
         <!-- Categories widget-->
-        <!-- <x-categories :users="$users"></x-categories> -->
-        <div class="card mb-4">
-            <div class="card-header">Categories</div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                    <!-- <ul class="list-unstyled mb-0">
-                        @foreach($users as $user)
-                        <li>
-                            <a href="#">{{$user->name}}</a>
-                        </li>
-                        @endforeach
-                    </ul> -->
-          </div>
-          <div class="col-sm-6">
-              <ul class="list-unstyled mb-0">
-                  <li><a href="#!">JavaScript</a></li>
-                  <li><a href="#!">CSS</a></li>
-                  <li><a href="#!">Tutorials</a></li>
-              </ul>
-          </div>
-        </div>
-        </div>
-       </div>
+        <x-categories :users="$users"></x-categories> 
                     <!-- Side widget-->
         <x-side-widget></x-side-widget>
                    
